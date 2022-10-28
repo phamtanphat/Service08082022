948a6eaeb063e1f5ac01d4e1719607585bf718f5
Phat <phatdroid94@gmail.com>
2022-10-28 20:13:56 +0700

    update

--- app/src/main/java/com/example/service08082022/MainActivity.java
+++ app/src/main/java/com/example/service08082022/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.service08082022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnStartService, btnStopService;
+    TextView tvCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnStartService = findViewById(R.id.button_start_service);
         btnStopService = findViewById(R.id.button_stop_service);
+        tvCount = findViewById(R.id.text_view_count);
 
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, MyService.class);
                 intent.putExtra("name", "phat");
                 startService(intent);
+                bindService(intent, connection, Context.BIND_AUTO_CREATE);
             }
         });
 
@@ -35,4 +43,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MyService.MyBinder myBinder = (MyService.MyBinder) iBinder;
+            myBinder.getService().setOnListenerValueChange(new MyService.OnListenValueChange() {
+                @Override
+                public void onChanged(int i) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvCount.setText("Count " + i);
+                        }
+                    });
+                }
+            });
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 }
